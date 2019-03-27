4be979078a32184937cc7976ca9afbcd88b5cef7
Habeeb Mac-iver <habeeb.maciver@aiesec.net>
2019-03-27 15:19:34 +0100

    Added login method in store

--- src/stores/index.js
+++ src/stores/index.js
@@ -5,8 +5,30 @@ import Api from '@/utils/API'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  actions: {},
-  mutations: {},
+  state: {
+    admin_token: {}
+  },
+  actions: {
+    async loginAdmin({ commit }, payload) {
+      try {
+        let res = await Api.post('auth/login', payload)
+        if(res.status == "success") {
+          commit("setAdmin", res.data.token)
+          return true
+        }else {
+          let response = { status: res.status, message: res.data }
+          return response
+        }
+      }catch(e) {
+        let response = { status: "fail", message: e }
+        return response
+      }
+    }
+  },
+  mutations: {
+    setAdmin(state, token) {
+      state.admin_token = token
+    }
+  },
   getters: {}
 })
